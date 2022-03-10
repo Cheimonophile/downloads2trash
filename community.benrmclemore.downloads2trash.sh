e267e00e8793bdbc8b18fab77b9ce0d4814e7827
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2022-03-09 19:23:53 -0700

    Fix spaces being turned into newlines

--- community.benrmclemore.downloads2trash.sh
+++ community.benrmclemore.downloads2trash.sh
@@ -1,4 +1,4 @@
-#!/bin/sh +ex
+#!/bin/sh -e
 
 # Constants
 DOWNLOADS=~/Downloads
@@ -9,20 +9,20 @@ TRASH=~/.Trash
 echo "--- $(date)"
 
 # mdfind -ctime +7
-find "$DOWNLOADS" -ctime +7 -depth 1 | while read NAME
+find ${DOWNLOADS} -ctime +7 -depth 1 | while read NAME
 do
   # skip if is downloads
-  if [ "$DOWNLOADS" = "$NAME" ]
+  if [[ ${DOWNLOADS} == ${NAME} ]]
   then
     continue
   fi
 
   # init new name
-  BASENAME=$(basename $NAME)
-  NEW_NAME="$TRASH/$BASENAME"
+  BASENAME="${NAME##*/}"
+  NEW_NAME="${TRASH}/${BASENAME}"
 
   # make sure it isn't DS_Store
-  if ["$BASENAME" = "$DS_STORE"]
+  if [[ ${BASENAME} == ${DS_STORE} ]]
   then
     continue
   fi
@@ -31,11 +31,11 @@ do
   I=1
   while [ -f "$NEW_NAME" ] || [ -d "$NEW_NAME" ]
   do
-    NEW_NAME="$TRASH/${BASENAME%%.*} ($I).${BASENAME#*.}"
+    NEW_NAME="$TRASH/($I) $BASENAME"
     I=$(($I+1))
   done
 
   # move the file
   mv "$NAME" "$NEW_NAME"
-  echo "$NAME"
+  echo "mv \"$NAME\" \"$NEW_NAME\""
 done
